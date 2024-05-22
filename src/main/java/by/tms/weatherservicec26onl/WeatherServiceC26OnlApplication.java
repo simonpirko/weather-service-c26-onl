6eb071afa9ba352aac302e81c72081f265ae99fd
Simon Pirko <sipi7gm@gmail.com>
2024-05-22 16:00:24 +0300

    Initial commit

---
+++ src/main/java/by/tms/weatherservicec26onl/WeatherServiceC26OnlApplication.java
@@ -0,0 +1,13 @@
+package by.tms.weatherservicec26onl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherServiceC26OnlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherServiceC26OnlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/tms/weatherservicec26onl/WeatherServiceC26OnlApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tms.weatherservicec26onl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherServiceC26OnlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
