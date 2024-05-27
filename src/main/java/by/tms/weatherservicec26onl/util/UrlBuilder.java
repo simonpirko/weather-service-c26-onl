983cb43c3cecd60a4826e3f3c649359e8908a316
ddumaa <dimaduma62@gmail.com>
2024-05-27 17:07:57 +0300

    cleaning

--- src/main/java/by/tms/weatherservicec26onl/util/UrlBuilder.java
+++
@@ -1,8 +0,0 @@
-package by.tms.weatherservicec26onl.util;
-
-import by.tms.weatherservicec26onl.constants.weatherConstants;
-import org.springframework.web.util.UriComponentsBuilder;
-
-public class UrlBuilder {
-
-}
