8b99ec567fbeebcb8ad5a3b1ddc7228f6736300f
Shifernik <m.Disai@mail.ru>
2024-06-05 22:24:42 +0300

    add security

--- src/main/java/by/tms/weatherservicec26onl/service/UserDetailsServiceImpl.java
+++ src/main/java/by/tms/weatherservicec26onl/service/UserDetailsServiceImpl.java
@@ -2,14 +2,10 @@ package by.tms.weatherservicec26onl.service;
 
 import by.tms.weatherservicec26onl.entity.User;
 import by.tms.weatherservicec26onl.repository.UserRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
