79e5d8843cc2ab96e91f511531f7a7d74a500dd0
Shifernik <m.Disai@mail.ru>
2024-06-05 22:24:21 +0300

    add security

---
+++ src/main/java/by/tms/weatherservicec26onl/service/UserDetailsServiceImpl.java
@@ -0,0 +1,36 @@
+package by.tms.weatherservicec26onl.service;
+
+import by.tms.weatherservicec26onl.entity.User;
+import by.tms.weatherservicec26onl.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("Пользователь не найден");
+        }
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.get().getUsername())
+                .password(user.get().getPassword())
+                .build();
+    }
+
+}
