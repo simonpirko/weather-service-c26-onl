29cb474ca11d196b2fbd273ea4afc4731c145dbd
ddumaa <dimaduma62@gmail.com>
2024-06-06 22:49:31 +0300

    rollback

--- src/main/java/by/tms/weatherservicec26onl/web/controller/UserController.java
+++ src/main/java/by/tms/weatherservicec26onl/web/controller/UserController.java
@@ -1,7 +1,7 @@
 package by.tms.weatherservicec26onl.web.controller;
 
-//import by.tms.weatherservicec26onl.auth.TokenProvider;
-//import by.tms.weatherservicec26onl.dto.LoginUserDto;
+import by.tms.weatherservicec26onl.auth.TokenProvider;
+import by.tms.weatherservicec26onl.dto.LoginUserDto;
 import by.tms.weatherservicec26onl.dto.RegistrationUserDto;
 import by.tms.weatherservicec26onl.entity.User;
 import by.tms.weatherservicec26onl.mapper.UserMapper;
@@ -19,18 +19,18 @@ public class UserController {
 
     private final UserService userService;
     private final UserMapper userMapper;
-    //private final TokenProvider tokenProvider;
+    private final TokenProvider tokenProvider;
 
     @PostMapping("/registration")
     public User registration(@RequestBody RegistrationUserDto dto) {
         return userService.add(userMapper.registrationUserDtoToUser(dto));
     }
 
-    /**
+
      @PostMapping("/login")
     public String login(@RequestBody LoginUserDto dto) {
         User user = userService.findByUsernameAndPassword(dto.getUsername(), dto.getPassword());
         return tokenProvider.generateToken(user);
     }
-     */
+
 }
