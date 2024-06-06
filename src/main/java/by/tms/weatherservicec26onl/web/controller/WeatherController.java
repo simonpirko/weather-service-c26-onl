38aefdb135f69aa9f373d239a816adffedc2fa94
ddumaa <dimaduma62@gmail.com>
2024-06-06 21:38:19 +0300

    comm

--- src/main/java/by/tms/weatherservicec26onl/web/controller/WeatherController.java
+++ src/main/java/by/tms/weatherservicec26onl/web/controller/WeatherController.java
@@ -1,23 +1,30 @@
 package by.tms.weatherservicec26onl.web.controller;
 
+
 import by.tms.weatherservicec26onl.model.WeatherResponse;
 import by.tms.weatherservicec26onl.service.WeatherService;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/weather")
-@AllArgsConstructor
 public class WeatherController {
 
-    private final WeatherService weatherService;
+    @Autowired
+    private WeatherService weatherService;
 
-    @GetMapping
-    public WeatherResponse getWeather(@RequestParam String apiKey) {
-        WeatherResponse weather = weatherService.getWeather();
-        return weather;
+    @GetMapping("/now/{location}")
+    public ResponseEntity<WeatherResponse> now(@PathVariable String location) {
+        return weatherService.getWeatherData(location);
     }
+
+    @GetMapping("/forecast/{location}/{days}")
+    public ResponseEntity<WeatherResponse> forecast(@PathVariable String location, @PathVariable String days) {
+        return weatherService.getWeatherData(location, days);
+    }
+
 }
